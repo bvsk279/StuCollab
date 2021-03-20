7be2d6e331e096b1a0965324332aec4d911bf31c
Aditya Agarwal <aditya.7@outlook.com>
2021-03-20 18:05:21 +0530

    Modified navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
+
+	$("#v-pills-myprojects-tab").hide();
+	$("#v-pills-otherprojects-tab").hide();
+
     $('#v-pills-projects-tab').click(function(){
     	$("#v-pills-myprojects-tab").toggle();
     	$("#v-pills-otherprojects-tab").toggle();
+    	$('#v-pills-projects-tab').toggleClass("nav-bg");
     })
 });
 
